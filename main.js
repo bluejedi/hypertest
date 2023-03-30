45b579eb669988cf4e8936202e781660883d9003
bluejedi <jedi_biru@yahoo.co.uk>
2023-03-30 21:33:29 +0700

    update template to hyperapp logo and counter

--- main.js
+++ main.js
@@ -1,23 +1,31 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './lib/main.js'
+import hyperappLogo from './hyperapp.svg'
+import { app } from 'hyperapp'
+import html from  'hyperlit'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+const view = state => html`
+    <div id="app">
+      <a href="https://vitejs.dev" target="_blank">
+        <img src="/vite.svg" class="logo" alt="Vite logo" />
+      </a>
+      <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+        <img src="${hyperappLogo}" class="logo vanilla" alt="JavaScript logo" />
+      </a>
+      <h1>Hello Hyperapp</h1>
+      <div class="card">
+        <h1>${state}</h1>
+        <button onclick=${state => state - 1}>-</button>
+        <button onclick=${state => state + 1}>+</button>
+      </div>
+      <p class="read-the-docs">
+        Click on the Vite logo to learn more
+      </p>      
     </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+  `
+app({
+  init: 0,
+  view: view,
+  node: document.getElementById('app')
+})    
 
-setupCounter(document.querySelector('#counter'))
+//setupCounter(document.querySelector('#counter'))
