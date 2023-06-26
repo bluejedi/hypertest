15987745f87bc916c0d90c623ec6e515bf55e294
bluejedi <jedi_biru@yahoo.co.uk>
2023-06-26 23:00:56 +0700

    refine profile

--- views/Layout.jsx
+++ views/Layout.jsx
@@ -8,23 +8,43 @@ const okClick = (_, event) => {
   return [auth.logout, auth]
 }
 
-// // Define the different pages for our app
-// const NavBar = (state, props) =>
-//     <ul class="tab tab-block">
-//         <li class="tab-item"> <a href="/"> Home</a></li>
-//         <li class= "tab-item"> <a href="/movies"> Movies</a></li>
-//         <div>{state.auth.key?<div><span class="chip">{state.auth.username}</span><button class="btn">Logout</button></div>:<li class= "tab-item"><a href="/login"> Login</a></li>}</div>
-//     </ul>
-
 const Viewz = (props, content) => <div>
-    <ul class="tab tab-block">
-        <li class="tab-item"> <a href="/"> Home</a></li>
-        <li class= "tab-item"> <a href="/movies"> Movies</a></li>
-        <li class= "tab-item"> <a href="/genres"> Genres</a></li>
-        <li class= "tab-item"> <a href="/people"> People</a></li>
-        <li class= "tab-item"> <a href="/jobs"> Jobs</a></li>
-        <div>{props.key?<div><span class="chip">{props.username}</span><button class="btn" onclick={okClick}>Logout</button></div>:<li class= "tab-item"><a href="/login"> Login</a></li>}</div>
-    </ul>
+    
+    <header class="navbar">
+      <section class="navbar-section">
+        <h1 class="navbar-brand text-bold mr-2">JAV HUB</h1>
+      </section>
+      <section class="navbar-center">        
+        <a href="/" class="btn btn-link"> Home</a>
+        <a href="/movies" class="btn btn-link"> Movies</a>
+        <a href="/genres" class="btn btn-link"> Genres</a>
+        <a href="/people" class="btn btn-link"> People</a>
+        <a href="/jobs" class="btn btn-link"> Jobs</a>
+      </section>
+      <section class="navbar-section"></section>
+    </header>
+    <section class="profile">
+        {props.key?<details class="accordion">
+      <summary class="accordion-header">            
+        <span class="chip"><figure class="avatar mr-2" data-initial="A"></figure>{props.username}</span>
+        <i class="icon icon-arrow-right mr-1"></i>
+      </summary>
+      <div class="accordion-body p2 mx-2">
+        <ul class="menu">
+          Profile
+          <li class="divider" data-content="LINKS">
+          </li>
+          <li class="menu-item">
+            <a href="#" onclick={okClick}>
+              <i class="icon icon-link"></i> Logout
+            </a>
+          </li>
+          </ul>
+      </div>
+    </details>:<span class="chip"><figure class="avatar mr-2" data-initial="G"></figure><a href="/login"> Login</a></span>}        
+    
+  </section>
+
     {content}
 </div>
 
