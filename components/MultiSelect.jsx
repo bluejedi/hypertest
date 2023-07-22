fa3bf797c94a1e77e8f3ab0afc97f7735797bdfd
bluejedi <jedi_biru@yahoo.co.uk>
2023-07-22 22:23:53 +0700

    refine auto complete with focused

--- components/MultiSelect.jsx
+++ components/MultiSelect.jsx
@@ -1,80 +1,29 @@
-import { text } from 'hyperapp'
-/*
-<input class="form-input" type="text" placeholder="" value=""
-                    //oninput= {(_,e) => load(`http://localhost:5000/api/genres/?name=${e.target.value}`, field.actions)}
-    oninput = {(state, e) => { return [
-        { ...state,
-            loading: true, //mandatory
-            //gitems: gitems,
-        },
-        load(`http://localhost:5000/api/genres/?name=${e.target.value}`, field.act)
-      ]}}
-/>*/
+//import { h } from 'hyperapp'
 
-const load= (url, act) => {
-    //console.log(act);
-    return dispatch => { 
-      fetch(url)
-      .then(response => response.json())
-      .then(data => dispatch([act, {response: data}]))
-    }
-};
-
-// from  @sergey-shpak sergey-shpak/hyperapp-stateful-component-example.js
-// https://gist.github.com/sergey-shpak/dffb81833fa060ec81d5830a9a039f82
-
-const Stateful = (Component, state) => 
- Component.bind(state || Component.state || {})
-
-// function loadz(url){
-//     //console.log(act);
-//     //return dispatch => { 
-//       fetch(url)
-//       .then(response => response.json())
-//       .then(data => update({response: data}))
-//     //}
-// };
-
-function onFocus(state){
-  //console.log('focus')
-  this.focused = true
-  return { ...state }
-}
-
-function onBlur(state){
-  //console.log('blur')
-  this.focused = false
-  return { ...state }
-}
-
-// function onInput(state, e){
-//   console.log('input')
-//   this.value =  e.target.value
-//   loadz(`http://localhost:5000/api/genres/?name=${e.target.value}`, update)
-//   return { ...state }
-// }
-
-function Input(props, children){
-  return h('div', {class:"has-icon-left"}, [
-    h('input', {
-      class: "form-input", type: "text",  
-      onfocus: onFocus.bind(this),
-      onblur: onBlur.bind(this),
-      oninput: props.onInput.bind(this),
-      //update: update.bind(this),
-      value: this.value
-    }),
-    this.loading && h('i', {class:"form-icon loading"})
-  ])
-}
 let citems = []
+let focused = false
 
-function Ilist(props, children) {
+const onFocus = (state) => {
+  console.log('focus')
+  focused = true
+  return { ...state }
+}
+
+const onBlur = (state) => {
+  console.log('blur')
+  focused = false
+  return { ...state }
+}
+
+const Ilist = (props, children) => {
     return <ul class="menu">
     { citems.map(v => 
-      <li class="menu-item" data-id={v.id}><button type="buton" class="btn btn-block bg-secondary btn-link text-left" onclick = {(_, e) => {
+      <li class="menu-item" data-id={v.id}>
+      <button type="buton" class="btn btn-block bg-secondary btn-link text-left" 
+      onclick = {(_, e) => {
             e.preventDefault();
             //console.log(e);
+            focused = false
             let newv = props.valz.concat({id: v.id, name: v.name})
             return props.actionz(newv)
             //e.preventDefault();
@@ -89,15 +38,38 @@ function Ilist(props, children) {
     </ul>   
 }
 
-// Exposing initial component state
-Input.state = { focused: false, value:"", loading: false, itemz:[] }
+const load= (url, act) => {
+    //console.log(act);
+    return dispatch => { 
+      fetch(url)
+      .then(response => response.json())
+      .then(data => dispatch([act, {response: data}]))
+    }
+}
 
-const InputBox1 = Stateful(Input, {focused: true, value: "", loading: false, itemz:[{id: 1, name: 'hc'},{id: 2, name: 'gb'}]})
+let loading = false
+let value = ""
 
-const MultiSelect = ({label, field, action, act}) =>
+const Input = (props, children) => {
+  return h('div', {class:"has-icon-left"}, [
+    h('input', {
+      class: "form-input", type: "text",
+      loading: loading,  
+      onfocus: onFocus.bind(this),
+      onblur: onBlur.bind(this),
+      oninput: props.onInput.bind(this),
+      //update: update.bind(this),
+      value: value
+    }),
+    loading && h('i', {class:"form-icon loading"})
+  ])
+}
+
+const MultiSelect = ({label, field, action}) =>
     <div class="form-group">
+    <label class="form-label" for="{field.label}">{field.label}</label>
       <div class="form-autocomplete">
-        <div class="form-autocomplete-input form-input is-focused">
+        <div class={`form-autocomplete-input form-input ${focused?'is-focused':''}`}>
             {field.value && field.value.map(v => 
                 <span class="chip" data-id={v.id}>{v.name}
                 <button type="button" class="btn btn-clear" onclick = {(_, e) => {
@@ -110,23 +82,20 @@ const MultiSelect = ({label, field, action, act}) =>
                 ></button>
                 </span>)}
               
-                {h(InputBox1, {focused: true, value:"",
-                    onInput: function (state, e) { this.loading= true; this.value = e.target.value; return [
-                        { ...state,
-                            loading: true, //mandatory
-                            //gitems: gitems,
-                        },
+                {h(Input, {value:"",
+                    onInput: (state, e) => { focused=true; loading= true; value = e.target.value; 
+                    return [
+                        { ...state},
                         load(`${field.url}=${e.target.value}`, (state, {response}) => { 
                             //console.log(state);
                             citems = response.results
-                            this.loading = false; return ({
-                            ...state}
-                        )})
+                            loading = false 
+                            return ({...state})})
                       ]} 
                 })}
                 
         </div>
-        {h(Ilist, {valz: field.value, actionz: action})}
+        {focused && h(Ilist, {valz: field.value, actionz: action})}
       </div>
     </div>
 
